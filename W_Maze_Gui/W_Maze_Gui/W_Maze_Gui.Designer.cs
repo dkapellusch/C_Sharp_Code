3946182cfffa62773f92b93bbe5bd9c13516aded
Daniel Kapellusch <dkapellusch@gmail.com>
2016-07-31 12:31:24 -0700

    design

--- W_Maze_Gui/W_Maze_Gui/W_Maze_Gui.Designer.cs
+++ W_Maze_Gui/W_Maze_Gui/W_Maze_Gui.Designer.cs
@@ -414,6 +414,7 @@
             this.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "W_Maze_Gui";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "W-Maze";
