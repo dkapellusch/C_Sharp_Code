0118f5448f4f6d0336edcf62a38da4965fa6f038
Daniel Kapellusch <dkapellusch@gmail.com>
2016-08-21 20:19:07 -0700

    fixed scaling

--- W_Maze_Gui/W_Maze_Gui/FillFeederWin.Designer.cs
+++ W_Maze_Gui/W_Maze_Gui/FillFeederWin.Designer.cs
@@ -46,18 +46,20 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(218, 25);
+            this.label1.Location = new System.Drawing.Point(164, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(176, 32);
+            this.label1.Size = new System.Drawing.Size(137, 26);
             this.label1.TabIndex = 0;
             this.label1.Text = "Fill Feeders";
             // 
             // fill1
             // 
             this.fill1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.fill1.Location = new System.Drawing.Point(151, 111);
+            this.fill1.Location = new System.Drawing.Point(103, 90);
+            this.fill1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.fill1.Name = "fill1";
-            this.fill1.Size = new System.Drawing.Size(65, 47);
+            this.fill1.Size = new System.Drawing.Size(54, 38);
             this.fill1.TabIndex = 5;
             this.fill1.Text = "Fill F1";
             this.fill1.UseVisualStyleBackColor = true;
@@ -66,9 +68,10 @@
             // fill2
             // 
             this.fill2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.fill2.Location = new System.Drawing.Point(281, 111);
+            this.fill2.Location = new System.Drawing.Point(208, 91);
+            this.fill2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.fill2.Name = "fill2";
-            this.fill2.Size = new System.Drawing.Size(66, 46);
+            this.fill2.Size = new System.Drawing.Size(56, 37);
             this.fill2.TabIndex = 6;
             this.fill2.Text = "Fill F2";
             this.fill2.UseVisualStyleBackColor = true;
@@ -77,9 +80,10 @@
             // fill3
             // 
             this.fill3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.fill3.Location = new System.Drawing.Point(409, 110);
+            this.fill3.Location = new System.Drawing.Point(313, 89);
+            this.fill3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.fill3.Name = "fill3";
-            this.fill3.Size = new System.Drawing.Size(66, 48);
+            this.fill3.Size = new System.Drawing.Size(54, 39);
             this.fill3.TabIndex = 7;
             this.fill3.Text = "Fill F3";
             this.fill3.UseVisualStyleBackColor = true;
@@ -90,9 +94,10 @@
             this.button1.BackColor = System.Drawing.Color.Red;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(151, 111);
+            this.button1.Location = new System.Drawing.Point(102, 89);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(65, 47);
+            this.button1.Size = new System.Drawing.Size(55, 38);
             this.button1.TabIndex = 8;
             this.button1.Text = "STOP";
             this.button1.UseVisualStyleBackColor = false;
@@ -103,9 +108,10 @@
             this.button2.BackColor = System.Drawing.Color.Red;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(281, 111);
+            this.button2.Location = new System.Drawing.Point(208, 90);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(66, 47);
+            this.button2.Size = new System.Drawing.Size(56, 38);
             this.button2.TabIndex = 9;
             this.button2.Text = "STOP";
             this.button2.UseVisualStyleBackColor = false;
@@ -116,9 +122,10 @@
             this.button3.BackColor = System.Drawing.Color.Red;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(409, 110);
+            this.button3.Location = new System.Drawing.Point(313, 88);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(66, 48);
+            this.button3.Size = new System.Drawing.Size(54, 39);
             this.button3.TabIndex = 10;
             this.button3.Text = "STOP";
             this.button3.UseVisualStyleBackColor = false;
@@ -129,9 +136,10 @@
             this.button4.BackColor = System.Drawing.Color.RoyalBlue;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(509, 437);
+            this.button4.Location = new System.Drawing.Point(382, 355);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(110, 57);
+            this.button4.Size = new System.Drawing.Size(82, 46);
             this.button4.TabIndex = 11;
             this.button4.Text = "Done Filling";
             this.button4.UseVisualStyleBackColor = false;
@@ -140,19 +148,20 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(-23, 41);
+            this.pictureBox1.Location = new System.Drawing.Point(-17, 33);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(676, 466);
+            this.pictureBox1.Size = new System.Drawing.Size(507, 379);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
             // FillFeederWin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
-            this.ClientSize = new System.Drawing.Size(645, 519);
+            this.ClientSize = new System.Drawing.Size(484, 422);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.fill3);
             this.Controls.Add(this.fill2);
@@ -163,6 +172,7 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FillFeederWin";
             this.Text = "FillFeederWin";
             this.Load += new System.EventHandler(this.FillFeederWin_Load);
